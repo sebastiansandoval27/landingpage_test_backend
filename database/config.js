e62c11cdf8c4b70af82908cdd4f9be92bc438b93
sebastiansandoval27 <juan.sandovals@usantoto.edu.co>
2021-04-01 15:44:02 -0500

    Backend done

---
+++ database/config.js
@@ -0,0 +1,27 @@
+
+
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+
+
+  try {
+
+    await mongoose.connect( process.env.DB_CNN , {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    console.log("DB ONLINE")
+    
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error a la hora de inicializar la base de datos')
+  }
+
+}
+
+module.exports = {
+  dbConnection
+}
